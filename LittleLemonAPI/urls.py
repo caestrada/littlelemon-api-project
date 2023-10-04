9bbb4912c02d5958ecef8ead5acb543b4ade947c
Carlos A. Estrada <cestr008@gmail.com>
2023-10-04 09:44:48 -0400

    add class-based views

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('menu-items/', views.menu_items),
+    path("orders", views.OrderList.as_view()),
+    path("orders/<int:pk>", views.OrderDetail.as_view()),
+
 ]
--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -2,8 +2,25 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
-# Create your views here.
+from rest_framework.views import APIView
 
 @api_view(['GET', 'POST'])
 def menu_items(request):
     return Response('List of menu items', status=status.HTTP_200_OK)
+
+class OrderList(APIView):
+    def get(self, request):
+        return Response('List of orders', status=status.HTTP_200_OK)
+
+    def post(self, request):
+        return Response('Order created', status=status.HTTP_201_CREATED)
+    
+class OrderDetail(APIView):
+    def get(self, request, pk):
+        return Response('Order detail with id ' + str(pk), status=status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        return Response('Order updated', status=status.HTTP_200_OK)
+
+    def delete(self, request, pk):
+        return Response('Order deleted', status=status.HTTP_204_NO_CONTENT)
