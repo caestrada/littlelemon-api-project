945a1fe321d0b90e05873fb00c7556f2d2951a92
Carlos A. Estrada <cestr008@gmail.com>
2023-10-07 12:48:35 -0400

    update authtoken url

--- LittleLemon/urls.py
+++ LittleLemon/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('api/', include('LittleLemonAPI.urls')),
     path("api/", include("djoser.urls")),
-    path("api/", include("djoser.urls.authtoken")),
+    path("", include("djoser.urls.authtoken")),
 ]
